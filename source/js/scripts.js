22d036620509d14d84e06af225ef7f7d3df47c7c
Igor Gnezdilov <gnezdilov.i.r@gmail.com>
2018-02-18 21:27:30 +0200

    Финальный вариант

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,16 +1,15 @@
 var mainNavWrapperToggle = document.querySelector(".main-nav__wrapper-nav");
-var pageHeaderjs = document.querySelector(".page-header_js");
 var pageHeadernojs = document.querySelector(".page-header_nojs");
 var pageHeader = document.querySelector(".page-header__wrapper");
 
 pageHeadernojs.classList.remove("page-header_nojs");
 pageHeadernojs.classList.add("page-header_js");
-pageHeader.classList.add("page-header_closed");
+pageHeader.classList.add("page-header__wrapper_closed");
 
 mainNavWrapperToggle.addEventListener("click", function(event) {
-  if(event.target.tagName === "BUTTON"){
+  if(event.target.tagName.toLowerCase() === "button") {
     event.preventDefault();
-    pageHeader.classList.toggle("page-header_opened");
-    pageHeader.classList.toggle("page-header_closed");
+    pageHeader.classList.toggle("page-header__wrapper_opened");
+    pageHeader.classList.toggle("page-header__wrapper_closed");
   }
 });
